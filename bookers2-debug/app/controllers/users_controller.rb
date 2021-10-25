83479e381b02f0f7ee8894947258735f045c2dd3
Tt6588 <tatsusato1011@gmail.com>
2021-10-25 03:33:09 +0000

    [fix]k-2

--- bookers2-debug/app/controllers/users_controller.rb
+++ bookers2-debug/app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
-class UsersController < ApplicationController
+class UsersController < ApplicationController 
+  before_action :authenticate_user!
   before_action :ensure_correct_user, only: [:update]
 
   def show
@@ -14,11 +15,16 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user == current_user
+      render :edit
+    else
+      redirect_to user_path(current_user.id)
+    end
   end
 
   def update
     if @user.update(user_params)
-      redirect_to users_path(@user), notice: "You have updated user successfully."
+      redirect_to users_path(current_user.id), notice: "You have updated user successfully."
     else
       render :edit
     end
